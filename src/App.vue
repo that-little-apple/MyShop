1299eb8290e722e8b23cb08fb0326a0b33124073
xiaoyao62 <xiaoyao62@jd.com>
2021-07-23 13:50:30 +0800

    watch方法使用

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,19 @@
     <router-view/>
 
     <mt-tabbar v-model="selected">
-      <mt-tab-item id="外卖">
+      <mt-tab-item id="Home">
         <img slot="icon" src="./assets/首页.png">
-        外卖
+        首页
       </mt-tab-item>
-      <mt-tab-item id="订单">
+      <mt-tab-item id="Order">
         <img slot="icon" src="./assets/商品.png">
         订单
       </mt-tab-item>
-      <mt-tab-item id="发现">
+      <mt-tab-item id="ShopCart">
         <img slot="icon" src="./assets/购物车.png">
-        发现
+        购物车
       </mt-tab-item>
-      <mt-tab-item id="我的">
+      <mt-tab-item id="Mine">
         <img slot="icon" src="./assets/我的.png">
         我的
       </mt-tab-item>
@@ -33,14 +33,25 @@
 
 <script>
   //https://www.h5w3.com/9059.html
-export default {
-  name: 'App',
-  data(){
-    return {
-      selected:''
+  export default {
+    name: 'App',
+    data() {
+      return {
+        selected: ''
+      }
+    },
+    //watch见识selected,当selected中的值发生变化，会自动调用后面的方法
+    //当点击底部导航中菜单项时，selected这个属性会发生变化，同时watch监视到该属性变化，会自动调用同名watch函数
+    watch: {
+      selected: function name(newCompent, oldComment) {
+        //newCompent:存储当前点击的菜单项
+        //oldComment：存储前面点击的菜单项
+        console.log("newCompent", newCompent)
+        console.log("oldComment", oldComment)
+        console.log(this.selected)//返回的是组件当中id组件的值
+      }
     }
   }
-}
 </script>
 
 <style>
